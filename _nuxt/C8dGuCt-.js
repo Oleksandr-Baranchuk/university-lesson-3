1ae25ff52d12a5ba6823f1b1827d35160bfcf76d
Oleksandr-Baranchuk <oleksandr.baranchuk1@gmail.com>
2025-10-25 14:39:43 +0300

    Updates

--- _nuxt/C8dGuCt-.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./DlAUqK2U.js";import{c as t,o}from"./d250SSfK.js";const e={};function n(r,s){return o(),t("div",null," Contacts ")}const f=c(e,[["render",n]]);export{f as default};
---
+++ _nuxt/DS04WrFt.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DlAUqK2U.js";import{c as o,o as t}from"./DC7S4wCU.js";const c={};function r(n,a){return t(),o("div",null," About Page ")}const f=e(c,[["render",r]]);export{f as default};
