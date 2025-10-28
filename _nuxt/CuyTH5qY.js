f2241e5a092b5c48e29c22193ad42a21ab29e87f
Oleksandr-Baranchuk <oleksandr.baranchuk1@gmail.com>
2025-10-28 17:16:06 +0200

    Updates

---
+++ _nuxt/CuyTH5qY.js
@@ -0,0 +1,1 @@
+import{z as o}from"./kfcw7JiG.js";const r=()=>o("color-mode").value;export{r as u};
--- _nuxt/CwHFKG6X.js
+++
@@ -1,1 +0,0 @@
-import{z as o}from"./XX9_9OfY.js";const r=()=>o("color-mode").value;export{r as u};
